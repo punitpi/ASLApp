a30779f5bb80e1ef8eb2c6a2f844ffcea61164d6
punitpi <punit.pi@gmail.com>
2019-03-05 13:15:52 +0530

    changes to app.run

--- main.py
+++ main.py
@@ -42,6 +42,4 @@ def move_forward():
 
 
 if __name__ == '__main__':
-	app.debug = True
-	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
+    app.run(host='localhost', debug=True)
