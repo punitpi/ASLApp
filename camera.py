86bffdfe419a92d13f274ee84a2cf3d9803d5c09
puneeth <punit.pi@gmail.com>
2019-04-25 08:08:06 +0530

    Cropping the image before predicting

--- camera.py
+++ camera.py
@@ -6,26 +6,10 @@ import sqlite3
 import tensorflow as tf
 import sys
 from keras.models import load_model
+from PIL import Image
 
 
 class VideoCamera(object):
-    def get_frame(self):
-        success, image = self.video.read()
-
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
-        img = cv2.flip(image, 1)
-        x1, y1, x2, y2 = 100, 100, 300, 300
-        cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,0), 2)
-
-        
-        img_cropped = img[y1:y2, x1:x2]
-        self.data = cv2.imencode('.jpg', img_cropped)[1].tostring()
-
-        ret, jpeg = cv2.imencode('.jpg', img)
-        return jpeg.tobytes()
-
     def get_predictions(self):
         with tf.gfile.FastGFile("logs/trained_graph.pb", 'rb') as f:
             graph_def = tf.GraphDef()
@@ -34,15 +18,16 @@ class VideoCamera(object):
 
         with tf.Session() as sess:
             image_path = "temp.jpg"
+            crop(image_path, image_path)
             image_data = tf.gfile.FastGFile(image_path, 'rb').read()
-            os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+            os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
             label_lines = [line.rstrip() for line
-                   in tf.gfile.GFile("logs/trained_labels.txt")]
+                           in tf.gfile.GFile("logs/trained_labels.txt")]
             softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
             # Read the image_data
             # Loads label file, strips off carriage return
-            predictions = sess.run(softmax_tensor, \
-                {'DecodeJpeg/contents:0': image_data})
+            predictions = sess.run(softmax_tensor,
+                                   {'DecodeJpeg/contents:0': image_data})
 
             # Sort to show labels of first prediction in order of confidence
             top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
@@ -57,3 +42,12 @@ class VideoCamera(object):
                     res = human_string
             return res, max_score
 
+def crop(image_path, saved_location):
+    """
+    @param image_path: The path to the image to edit
+    @param coords: A tuple of x/y coordinates (x1, y1, x2, y2)
+    @param saved_location: Path to save the cropped image
+    """
+    image_obj = Image.open(image_path)
+    cropped_image = image_obj.crop((0, 0, 200, 200))
+    cropped_image.save(saved_location)
